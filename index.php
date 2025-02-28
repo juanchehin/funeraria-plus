809437ded900628e0f736817092397100e027075
juanchehin <chehinjuan@gmail.com>
2025-02-28 11:12:31 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
+	$currDir = dirname(__FILE__);
+
+
+	include("{$currDir}/defaultLang.php");
+	include("{$currDir}/language.php");
+	include("{$currDir}/lib.php");
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+
+
+	$x = new DataList;
+	$x->TableTitle = $Translation['homepage'];
+	$tablesPerRow = 2;
+	$arrTables = getTableList();
+
+	// according to provided GET parameters, either log out, show login form (possibly with a failed login message), or show homepage
+	if(isset($_GET['signOut'])){
+		logOutUser();
+		redirect("index.php?signIn=1");
+	}elseif(isset($_GET['loginFailed']) || isset($_GET['signIn'])){
+		if(!headers_sent() && isset($_GET['loginFailed'])) header('HTTP/1.0 403 Forbidden');
+		include("{$currDir}/login.php");
+	}else{
+		include("{$currDir}/header-user.php");
+	}
